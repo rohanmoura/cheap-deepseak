b323135d520b8384f573e7b06a947acb7e7c7ad9
rohanmoura <rsmoura29@gmail.com>
2025-03-21 17:33:01 +0530

    Adding authentication functionality

--- deepseak/components/Sidebar.jsx
+++ deepseak/components/Sidebar.jsx
@@ -1,8 +1,13 @@
 import { assets } from "@/assets/assets";
+import { useAppContext } from "@/context/AppContext";
+import { useClerk, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import React from "react";
 
 const Sidebar = ({ expand, setExpand }) => {
+  const { openSignIn } = useClerk();
+  const { user } = useAppContext();
+
   return (
     <div
       className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${
@@ -112,11 +117,16 @@ const Sidebar = ({ expand, setExpand }) => {
           )}
         </div>
         <div
+          onClick={user ? null : openSignIn}
           className={`flex items-center ${
             expand ? "hover:bg-white/10 rounded-lg" : "justify-center w-full"
           } gap-3 text-white/60 text-sm p-2 mt-2 cursor-pointer`}
         >
-          <Image src={assets.profile_icon} alt="" className="w-7" />
+          {user ? (
+            <UserButton />
+          ) : (
+            <Image src={assets.profile_icon} alt="" className="w-7" />
+          )}
           {expand && <span>My Profile</span>}
         </div>
       </div>
